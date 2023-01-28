4f6f4df5d83105ee9e0eeeffe0f7ade2a7991824
Abhay Bairagi <101196045+AbhayNB@users.noreply.github.com>
2023-01-28 13:19:45 +0530

    Update settings.py

--- nariconfort/settings.py
+++ nariconfort/settings.py
@@ -83,8 +83,12 @@ WSGI_APPLICATION = 'nariconfort.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': '7vwkqj4aDJ28prsUwpDP',
+        'HOST': 'containers-us-west-155.railway.app',
+        'PORT': '7114'
     }
 }
 
